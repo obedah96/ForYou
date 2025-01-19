b8db79b912e44b84a4ca5870919a3395940f888b
obedah shaban <95497072+obedah96@users.noreply.github.com>
2025-01-19 01:19:25 -0800

    edit table

--- app/Domain/Entities/RealEstate.php
+++ app/Domain/Entities/RealEstate.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class RealEstate extends Model
 {
+    protected $table = 'real_estate';
     protected $fillable = [
         'product_id', 'product_name', 'product_city', 'product_count',
         'description', 'price','image_path'
