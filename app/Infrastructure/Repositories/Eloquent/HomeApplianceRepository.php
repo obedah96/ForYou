7ded731ca8cc782ecc0e0e11e23f204ddd186180
obedah shaban <95497072+obedah96@users.noreply.github.com>
2025-01-19 02:29:05 -0800

    edit home repo

--- app/Infrastructure/Repositories/Eloquent/HomeApplianceRepository.php
+++ app/Infrastructure/Repositories/Eloquent/HomeApplianceRepository.php
@@ -10,15 +10,15 @@ class HomeApplianceRepository implements HomeApplianceRepositoryInterface
     public function paginate(int $limit, int $page)
     {
         if ($page == 1) {
-            return HomeAppliance::select('home_appliances.product_id', 'home_appliances.product_name', 'home_appliances.price', 'home_appliances.image_path', 'home_appliances.product_city', 'products.product_type') // إضافة نوع المنتج من جدول products
-                                ->join('products', 'home_appliances.product_id', '=', 'products.id') // الانضمام إلى جدول products عبر product_id
-                                ->latest()
+            return HomeAppliance::select('home_appliances.product_id', 'home_appliances.product_name', 'home_appliances.price', 'home_appliances.image_path', 'home_appliances.product_city', 'products.product_type') 
+                                ->join('products', 'home_appliances.product_id', '=', 'products.id') 
+                                ->orderBy('home_appliances.created_at', 'desc')
                                 ->take(3)
                                 ->get();
         }
         
-        return HomeAppliance::select('home_appliances.product_name', 'home_appliances.price', 'home_appliances.image_path', 'home_appliances.product_city', 'products.product_type') // إضافة نوع المنتج من جدول products
-                            ->join('products', 'home_appliances.product_id', '=', 'products.id') // الانضمام إلى جدول products
+        return HomeAppliance::select('home_appliances.product_name', 'home_appliances.price', 'home_appliances.image_path', 'home_appliances.product_city', 'products.product_type') 
+                            ->join('products', 'home_appliances.product_id', '=', 'products.id') 
                             ->paginate($limit, ['*'], 'page', $page);
         
     }
