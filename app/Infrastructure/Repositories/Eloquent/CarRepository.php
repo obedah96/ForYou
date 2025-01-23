11fafc205fc5de5ce0ee6976c6ddfe27e42ebd70
obedah shaban <95497072+obedah96@users.noreply.github.com>
2025-01-23 08:13:00 -0800

    edit get cars

--- app/Infrastructure/Repositories/Eloquent/CarRepository.php
+++ app/Infrastructure/Repositories/Eloquent/CarRepository.php
@@ -7,7 +7,7 @@ use App\Infrastructure\Repositories\Interfaces\CarRepositoryInterface;
 
 class CarRepository implements CarRepositoryInterface
 {
-    public function getLatestCars(int $perPage)
+   /* public function getLatestCars(int $perPage)
     {
         return Car::select('cars.product_id', 'cars.product_name', 'cars.price', 'cars.image_path', 'cars.product_city', 'products.product_type')
         ->join('products', 'cars.product_id', '=', 'products.id') 
@@ -16,4 +16,29 @@ class CarRepository implements CarRepositoryInterface
 
 
     }
+*/
+        public function getLatestCars(int $perPage)
+        {
+            if ($perPage === 1) {
+                
+                return Car::select('cars.product_id', 'cars.product_name', 'cars.price', 'cars.image_path', 'cars.product_city', 'products.product_type')
+                    ->join('products', 'cars.product_id', '=', 'products.id')
+                    ->orderBy('cars.created_at', 'desc')
+                    ->limit(3) 
+                    ->get();
+            } 
+            elseif ($perPage === 2) {
+                
+                return Car::select('cars.product_id', 'cars.product_name', 'cars.price', 'cars.image_path', 'cars.product_city', 'products.product_type')
+                    ->join('products', 'cars.product_id', '=', 'products.id')
+                    ->orderBy('cars.created_at', 'desc')
+                    ->skip(3) 
+                    ->take(PHP_INT_MAX)
+                    ->get();
+            } 
+            else {
+                return collect([]);
+            }
+        }
+
 }
