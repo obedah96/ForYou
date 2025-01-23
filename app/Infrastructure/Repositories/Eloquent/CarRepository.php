63aec8d75677a2266c236d804d1f57102e819ba3
obedah shaban <95497072+obedah96@users.noreply.github.com>
2025-01-23 08:24:27 -0800

    edit get cars

--- app/Infrastructure/Repositories/Eloquent/CarRepository.php
+++ app/Infrastructure/Repositories/Eloquent/CarRepository.php
@@ -15,30 +15,39 @@ class CarRepository implements CarRepositoryInterface
         ->paginate($perPage);
 
 
+    }*/
+
+    public function getLatestCars(int $perPage)
+    {
+        
+        $latestCars = Car::select('cars.product_id', 'cars.product_name', 'cars.price', 'cars.image_path', 'cars.product_city', 'products.product_type')
+            ->join('products', 'cars.product_id', '=', 'products.id')
+            ->orderBy('cars.created_at', 'desc')
+            ->take(3)
+            ->get();
+
+        
+        $otherCars = Car::select('cars.product_id', 'cars.product_name', 'cars.price', 'cars.image_path', 'cars.product_city', 'products.product_type')
+            ->join('products', 'cars.product_id', '=', 'products.id')
+            ->orderBy('cars.created_at', 'desc')
+            ->skip(3)
+            ->get();
+
+        
+        $allCars = $latestCars->merge($otherCars);
+
+        
+        $currentPage = \Illuminate\Pagination\Paginator::resolveCurrentPage();
+        $items = $allCars->forPage($currentPage, $perPage);
+
+        
+        return new \Illuminate\Pagination\LengthAwarePaginator(
+            $items, 
+            $allCars->count(), 
+            $perPage, 
+            $currentPage, 
+            ['path' => \Illuminate\Pagination\Paginator::resolveCurrentPath()]
+        );
     }
-*/
-        public function getLatestCars(int $perPage)
-        {
-            if ($perPage === 1) {
-                
-                return Car::select('cars.product_id', 'cars.product_name', 'cars.price', 'cars.image_path', 'cars.product_city', 'products.product_type')
-                    ->join('products', 'cars.product_id', '=', 'products.id')
-                    ->orderBy('cars.created_at', 'desc')
-                    ->limit(3) 
-                    ->get();
-            } 
-            elseif ($perPage === 2) {
-                
-                return Car::select('cars.product_id', 'cars.product_name', 'cars.price', 'cars.image_path', 'cars.product_city', 'products.product_type')
-                    ->join('products', 'cars.product_id', '=', 'products.id')
-                    ->orderBy('cars.created_at', 'desc')
-                    ->skip(3) 
-                    ->take(PHP_INT_MAX)
-                    ->get();
-            } 
-            else {
-                return collect([]);
-            }
-        }
 
 }
