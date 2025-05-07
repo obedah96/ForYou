38b8af63878c2ee8a754699fc1e2f5d356acc110
obedah shaban <95497072+obedah96@users.noreply.github.com>
2025-05-07 05:39:58 -0700

    edit dashboard home page

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,13 @@ Route::middleware([
 Route::get('dashboard',function(){
     return view('admin.dashboard'); 
 })->name('dashboard');
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::view('/dashboard/cars',            'admin.cars')            ->name('dashboard.cars');
+    Route::view('/dashboard/electrical',      'admin.electrical')      ->name('dashboard.electrical');
+    Route::view('/dashboard/home-appliances', 'admin.home-appliances') ->name('dashboard.home_appliances');
+    Route::view('/dashboard/real-estates',    'admin.real-estates')    ->name('dashboard.real_estates');
+});
 Route::view('/dashboard/home','admin.dashboard-home')->name('dashboard.home');
 
 Route::get('/', function () {
